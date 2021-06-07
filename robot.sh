d7f25a6e959cd4371afca4eb1bd1eef85b1bc930
Myfanqie <1752181917@qq.com>
2021-06-07 17:11:02 +0800

    脚本更新

--- robot.sh
+++ robot.sh
@@ -1,1 +1,7 @@
-nohup java -jar simbot-mirai-demo-2.0.5.jar  > log.file  2>&1 &
+if test -z "$(ps aux|grep simbot-mirai-demo |grep -v grep|cut -c 9-16|xargs)"; then
+	echo "The result is empty."
+else
+	ps aux|grep simbot-mirai-demo |grep -v grep|cut -c 9-16|xargs kill -15
+fi
+
+nohup java -jar simbot-mirai-demo-2.0.5.jar --simbot.yml  > log.file  2>&1 &
